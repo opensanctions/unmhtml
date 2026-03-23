79d432d5db34d53b44374b6a0b161315572ef6dc
Johan Schuijt <johan@resolve.works>
2026-03-23 15:36:57 +0100

    bump 0.3.0

--- unmhtml/__init__.py
+++ unmhtml/__init__.py
@@ -41,5 +41,5 @@ from .converter import MHTMLConverter
 from .parser import MHTMLParser
 from .processor import HTMLProcessor
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["MHTMLConverter", "MHTMLParser", "HTMLProcessor"]
